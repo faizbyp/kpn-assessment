2e31fa2871a2b2015f242debaf69495b44ea87ce
faizbyp <faizbyp@gmail.com>
2024-11-01 17:57:35 +0700

    update: usedialog hooks previous hook doesnt exchange props properly

--- src/hooks/useDialog.tsx
+++ src/hooks/useDialog.tsx
@@ -1,37 +1,18 @@
-import { ReactNode, useState } from "react";
-import DialogComp from "../components/Dialog";
+import { useState, useCallback } from "react";
 
 const useDialog = () => {
-  const [open, setOpen] = useState(false);
-  const [title, setTitle] = useState("");
-  const [actions, setActions] = useState<ReactNode>(null);
-  const [children, setChildren] = useState<ReactNode>(null);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const showDialog = (dialogTitle: string, dialogActions: ReactNode, dialogChildren: ReactNode) => {
-    setTitle(dialogTitle);
-    setActions(dialogActions);
-    setChildren(dialogChildren);
-    setOpen(true);
+  const open = useCallback(() => setIsOpen(true), []);
+  const close = useCallback(() => setIsOpen(false), []);
+  const toggle = useCallback(() => setIsOpen((prev) => !prev), []);
+
+  return {
+    isOpen,
+    open,
+    close,
+    toggle,
   };
-
-  const closeDialog = () => {
-    // if (reason && reason === "backdropClick") return;
-    setOpen(false);
-  };
-
-  const Dialog = (
-    <DialogComp
-      title={title}
-      actions={actions}
-      open={open}
-      onClose={closeDialog}
-      keepMounted={true}
-    >
-      {children}
-    </DialogComp>
-  );
-
-  return { Dialog, showDialog, closeDialog };
 };
 
 export default useDialog;
--- src/pages/BusinessUnit.tsx
+++ src/pages/BusinessUnit.tsx
@@ -1,6 +1,6 @@
 import { Button, IconButton, Typography } from "@mui/material";
 import StandardTable from "../components/StandardTable";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import useFetch from "../hooks/useFetch";
 import { TableSkeleton } from "../components/Skeleton";
 import EditIcon from "@mui/icons-material/Edit";
@@ -9,21 +9,24 @@ import useDialog from "../hooks/useDialog";
 import { snack } from "../providers/SnackbarProvider";
 import { API } from "../utils/api";
 import { useLoading } from "../providers/LoadingProvider";
+import DialogComp from "@/components/Dialog";
 
 const BusinessUnit = () => {
   const { showLoading, hideLoading } = useLoading();
   const { data: bu, refetch } = useFetch<any>("/bu");
-  const { Dialog, showDialog, closeDialog } = useDialog();
+  const [BU, setBU] = useState({ id: "", bu_name: "" });
+  const { isOpen: isOpenDelete, open: openDelete, close: closeDelete } = useDialog();
+
   const columns: any = useMemo(
     () => [
       {
-        header: "Code",
-        accessorKey: "code",
+        header: "BU Code",
+        accessorKey: "bu_code",
         cell: (props: any) => props.getValue(),
       },
       {
         header: "BU Name",
-        accessorKey: "name",
+        accessorKey: "bu_name",
         cell: (props: any) => props.getValue(),
       },
       {
@@ -37,12 +40,12 @@ const BusinessUnit = () => {
         meta: { align: "right" },
         cell: (props: any) => {
           const id = props.getValue();
-          const name = props.row.original.name;
+          const bu_name = props.row.original.bu_name;
 
           return (
             <>
               <IconButton
-                onClick={() => handleEdit(id)}
+                onClick={() => alert(id)}
                 aria-label="edit"
                 size="small"
                 edge="end"
@@ -51,7 +54,7 @@ const BusinessUnit = () => {
                 <EditIcon />
               </IconButton>
               <IconButton
-                onClick={() => handleOpenDelete(id, name)}
+                onClick={() => handleOpenDelete(id, bu_name)}
                 aria-label="delete"
                 color="error"
                 size="small"
@@ -67,26 +70,9 @@ const BusinessUnit = () => {
     []
   );
 
-  const handleOpenDelete = (id: string, name: string) => {
-    const actions = (
-      <>
-        <Button onClick={closeDialog} variant="outlined" color="error">
-          Cancel
-        </Button>
-        <Button onClick={() => handleDelete(id)} variant="contained" color="error">
-          Delete
-        </Button>
-      </>
-    );
-
-    const children = <Typography>{`Are you sure you want to delete ${name}?`}</Typography>;
-
-    showDialog("Delete Confirmation", actions, children);
-  };
-
-  const handleEdit = (id: string) => {
-    console.log("Edit clicked for id:", id);
-    alert(`Edit ${id}`);
+  const handleOpenDelete = (id: string, bu_name: string) => {
+    setBU({ id, bu_name });
+    openDelete();
   };
 
   const handleDelete = async (id: string) => {
@@ -100,7 +86,7 @@ const BusinessUnit = () => {
       console.error(error);
       snack.error(error as string);
     } finally {
-      closeDialog();
+      closeDelete();
       hideLoading();
     }
   };
@@ -115,7 +101,24 @@ const BusinessUnit = () => {
       ) : (
         <TableSkeleton column={4} row={2} small />
       )}
-      {Dialog}
+
+      <DialogComp
+        title="Delete BU"
+        open={isOpenDelete}
+        onClose={closeDelete}
+        actions={
+          <>
+            <Button onClick={closeDelete} variant="outlined" color="error">
+              Cancel
+            </Button>
+            <Button onClick={() => handleDelete(BU.id)} variant="contained" color="error">
+              Delete
+            </Button>
+          </>
+        }
+      >
+        <Typography>{`Are you sure you want to delete ${BU.bu_name}?`}</Typography>
+      </DialogComp>
     </>
   );
 };
