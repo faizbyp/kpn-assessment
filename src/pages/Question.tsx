78d9505747e7270440f53aa8748960bdc5fc2e58
faizbyp <faizbyp@gmail.com>
2024-11-25 17:51:36 +0700

    feat: question table

--- src/pages/Question.tsx
+++ src/pages/Question.tsx
@@ -1,9 +1,87 @@
-import { Button, Typography } from "@mui/material";
+import { Box, Button, IconButton, Typography } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import { useNavigate } from "react-router-dom";
+import useFetch from "@/hooks/useFetch";
+import StandardTable from "@/components/StandardTable";
+import { TableSkeleton } from "@/components/Skeleton";
+import { useMemo } from "react";
+import InfoIcon from "@mui/icons-material/Info";
 
 const Question = () => {
   const navigate = useNavigate();
+  const { data: question } = useFetch<any>("/question");
+
+  const columns: any = useMemo(
+    () => [
+      // {
+      //   header: "Seq.",
+      //   accessorKey: "q_seq",
+      //   cell: (props: any) => props.getValue(),
+      // },
+      {
+        header: "Layout",
+        accessorKey: "q_layout_type",
+        cell: (props: any) => props.getValue(),
+      },
+      {
+        header: "Question",
+        accessorKey: "q_input_text",
+        cell: (props: any) => props.getValue(),
+      },
+      {
+        header: "Question Image",
+        accessorKey: "q_input_image_url",
+        cell: (props: any) => (
+          <Box sx={{ height: 100, display: "flex", alignItems: "center" }}>
+            {props.getValue() ? (
+              <img
+                height={100}
+                src={`http://localhost:5000/api/static/question/${props.getValue()}`}
+                alt={props.row.original.q_input_text || "Cannot load image"}
+              />
+            ) : (
+              <Typography color="text.secondary" fontStyle="italic">
+                no image
+              </Typography>
+            )}
+          </Box>
+        ),
+      },
+      {
+        header: "Answer Type",
+        accessorKey: "answer_type",
+        cell: (props: any) => props.getValue(),
+      },
+      {
+        header: "Created By",
+        accessorKey: "created_by",
+        cell: (props: any) => props.getValue(),
+      },
+      {
+        header: "Action",
+        accessorKey: "id",
+        meta: { align: "right" },
+        cell: (props: any) => {
+          // const id = props.getValue();
+          // const bu_name = props.row.original.bu_name;
+
+          return (
+            <>
+              <IconButton
+                onClick={() => alert("laskdja")}
+                aria-label="edit"
+                size="small"
+                edge="end"
+              >
+                <InfoIcon />
+              </IconButton>
+            </>
+          );
+        },
+      },
+    ],
+    []
+  );
 
   return (
     <>
@@ -18,6 +96,12 @@ const Question = () => {
           Create Question
         </Button>
       </Typography>
+
+      {question ? (
+        <StandardTable columns={columns} data={question?.data} />
+      ) : (
+        <TableSkeleton column={4} row={2} small />
+      )}
     </>
   );
 };
